86240f1811b52419baf94fa2ead1ad2a230dc946
Murilo Kunze <murilokunze@hotmail.com>
2022-10-24 11:56:23 -0300

    First commit

---
+++ mongo/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using mongo.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mongo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IMongoCollection<BsonDocument> collection;
+
+        public HomeController(IMongoCollection<BsonDocument> collection)
+        {
+            this.collection = collection;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var filter = Builders<BsonDocument>.Filter.Empty;
+            var results = await collection.FindAsync(filter);
+
+            return View(results);
+        }
+    }
+}
